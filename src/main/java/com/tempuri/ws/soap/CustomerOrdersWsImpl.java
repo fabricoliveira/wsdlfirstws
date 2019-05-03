86449bd180db86b93201c02ddef225d5a8a7e404
fabriciooliveira <oliveira.fabricio.santos@gmail.com>
2019-05-03 11:55:35 -0300

    Creating an endpoint

---
+++ src/main/java/com/tempuri/ws/soap/CustomerOrdersWsImpl.java
@@ -0,0 +1,23 @@
+package com.tempuri.ws.soap;
+
+import com.tempuri.CreateOrdersRequest;
+import com.tempuri.CreateOrdersResponse;
+import com.tempuri.CustomerOrdersPortType;
+import com.tempuri.GetOrdersRequest;
+import com.tempuri.GetOrdersResponse;
+
+public class CustomerOrdersWsImpl implements CustomerOrdersPortType {
+
+	@Override
+	public GetOrdersResponse getOrders(GetOrdersRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CreateOrdersResponse createOrders(CreateOrdersRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
