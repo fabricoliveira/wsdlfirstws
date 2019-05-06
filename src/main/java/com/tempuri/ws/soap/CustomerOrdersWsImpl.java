2d289394123075d3362507a2d263a4126793dd64
fabriciooliveira <oliveira.fabricio.santos@gmail.com>
2019-05-06 09:44:15 -0300

    Implementing the init method.

--- src/main/java/com/tempuri/ws/soap/CustomerOrdersWsImpl.java
+++ src/main/java/com/tempuri/ws/soap/CustomerOrdersWsImpl.java
@@ -1,22 +1,55 @@
 package com.tempuri.ws.soap;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import com.tempuri.CreateOrdersRequest;
 import com.tempuri.CreateOrdersResponse;
 import com.tempuri.CustomerOrdersPortType;
 import com.tempuri.GetOrdersRequest;
 import com.tempuri.GetOrdersResponse;
+import com.tempuri.Order;
+import com.tempuri.Product;
 
 public class CustomerOrdersWsImpl implements CustomerOrdersPortType {
 
+	Map<BigInteger, List<Order>> customerOrders = new HashMap<>();
+	int currentId;
+	
+	public CustomerOrdersWsImpl() {
+		init();
+	}
+
+	public void init() {
+		List<Order> orders = new ArrayList<>();
+		Order order = new Order();
+		order.setId(BigInteger.valueOf(1));
+		
+		Product product = new Product();
+		product.setId("1");
+		product.setDescription("IPhone");
+		product.setQuantity(BigInteger.valueOf(3));
+		order.getProduct().add(product);
+		
+		customerOrders.put(BigInteger.valueOf(++currentId), orders);
+	}
+	
 	@Override
 	public GetOrdersResponse getOrders(GetOrdersRequest request) {
-		// TODO Auto-generated method stub
-		return null;
+		BigInteger customerId = request.getCustomerId();
+		List<Order> orders = customerOrders.get(customerId);
+		
+		GetOrdersResponse response = new GetOrdersResponse();
+		response.getOrder().addAll(orders);
+		
+		return response;
 	}
 
 	@Override
 	public CreateOrdersResponse createOrders(CreateOrdersRequest request) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
